b5cf054689c39fe7cbf20c21fb10ca81721b299c
Philipp A <flying-sheep@web.de>
2015-07-15 11:47:41 +0200

    switched to PyQt5 an QIcon.fromTheme

--- uic2.py
+++ uic2.py
@@ -3,7 +3,7 @@ from glob import glob
 from subprocess import call
 import os
 
-from PyQt4.uic import compileUi
+from PyQt5.uic import compileUi
 
 def ui():
     for uifile in glob("*.ui"):
@@ -19,7 +19,7 @@ def resource():
         pyfile = os.path.join('nbmanager', resfile[:-4] + "_rc.py")
         #if outdated(pyfile, resfile):
         print(resfile)
-        call("pyrcc4 -py3 -o {} {}".format(pyfile, resfile), shell=True)
+        call("pyrcc5 -o {} {}".format(pyfile, resfile), shell=True)
 
 if __name__ == "__main__":
     ui()
