dbef20de1c976ad2ed36f895e1bef65da16dd8ba
Philipp A <flying-sheep@web.de>
2015-07-20 09:16:52 +0200

    switched to PyQt5

--- nbmanager/ui_mainwindow.py
+++ nbmanager/ui_mainwindow.py
@@ -2,82 +2,67 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Tue Sep 23 13:15:45 2014
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt5 UI code generator 5.4.2
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(382, 488)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.treeView = QtGui.QTreeView(self.centralwidget)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.treeView = QtWidgets.QTreeView(self.centralwidget)
         self.treeView.setIconSize(QtCore.QSize(16, 16))
-        self.treeView.setObjectName(_fromUtf8("treeView"))
+        self.treeView.setObjectName("treeView")
         self.treeView.header().setVisible(False)
         self.verticalLayout.addWidget(self.treeView)
-        self.groupBox = QtGui.QGroupBox(self.centralwidget)
-        self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox)
-        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-        self.start_dir_lineedit = QtGui.QLineEdit(self.groupBox)
-        self.start_dir_lineedit.setObjectName(_fromUtf8("start_dir_lineedit"))
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.start_dir_lineedit = QtWidgets.QLineEdit(self.groupBox)
+        self.start_dir_lineedit.setObjectName("start_dir_lineedit")
         self.verticalLayout_2.addWidget(self.start_dir_lineedit)
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.choose_dir_button = QtGui.QPushButton(self.groupBox)
-        self.choose_dir_button.setObjectName(_fromUtf8("choose_dir_button"))
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.choose_dir_button = QtWidgets.QPushButton(self.groupBox)
+        self.choose_dir_button.setObjectName("choose_dir_button")
         self.horizontalLayout.addWidget(self.choose_dir_button)
-        self.launch_button = QtGui.QPushButton(self.groupBox)
+        self.launch_button = QtWidgets.QPushButton(self.groupBox)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/icons/right.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/icons/icons/right.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.launch_button.setIcon(icon)
-        self.launch_button.setObjectName(_fromUtf8("launch_button"))
+        self.launch_button.setObjectName("launch_button")
         self.horizontalLayout.addWidget(self.launch_button)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.verticalLayout.addWidget(self.groupBox)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 382, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.toolBar = QtGui.QToolBar(MainWindow)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
         self.toolBar.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
-        self.toolBar.setObjectName(_fromUtf8("toolBar"))
+        self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
-        self.actionShutdown = QtGui.QAction(MainWindow)
+        self.actionShutdown = QtWidgets.QAction(MainWindow)
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/icons/stop.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap(":/icons/icons/stop.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionShutdown.setIcon(icon1)
-        self.actionShutdown.setObjectName(_fromUtf8("actionShutdown"))
-        self.actionRefresh = QtGui.QAction(MainWindow)
+        self.actionShutdown.setObjectName("actionShutdown")
+        self.actionRefresh = QtWidgets.QAction(MainWindow)
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/icons/reload.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap(":/icons/icons/reload.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.actionRefresh.setIcon(icon2)
-        self.actionRefresh.setObjectName(_fromUtf8("actionRefresh"))
+        self.actionRefresh.setObjectName("actionRefresh")
         self.toolBar.addAction(self.actionShutdown)
         self.toolBar.addAction(self.actionRefresh)
 
@@ -85,14 +70,15 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "IPython nbmanager", None))
-        self.groupBox.setTitle(_translate("MainWindow", "Launch a server", None))
-        self.choose_dir_button.setText(_translate("MainWindow", "Choose directory", None))
-        self.launch_button.setText(_translate("MainWindow", "Launch", None))
-        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar", None))
-        self.actionShutdown.setText(_translate("MainWindow", "Shutdown", None))
-        self.actionShutdown.setToolTip(_translate("MainWindow", "<html><head/><body><p>Shut down the selected notebook server or session</p></body></html>", None))
-        self.actionRefresh.setText(_translate("MainWindow", "Refresh", None))
-        self.actionRefresh.setToolTip(_translate("MainWindow", "Refresh the list of processes", None))
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "IPython nbmanager"))
+        self.groupBox.setTitle(_translate("MainWindow", "Launch a server"))
+        self.choose_dir_button.setText(_translate("MainWindow", "Choose directory"))
+        self.launch_button.setText(_translate("MainWindow", "Launch"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.actionShutdown.setText(_translate("MainWindow", "Shutdown"))
+        self.actionShutdown.setToolTip(_translate("MainWindow", "<html><head/><body><p>Shut down the selected notebook server or session</p></body></html>"))
+        self.actionRefresh.setText(_translate("MainWindow", "Refresh"))
+        self.actionRefresh.setToolTip(_translate("MainWindow", "Refresh the list of processes"))
 
 from . import qtresources_rc
