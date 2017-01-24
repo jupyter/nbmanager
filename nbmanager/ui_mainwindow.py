e45dad30070d0af2fbb1f7042f5d6f13461684d7
Thomas Kluyver <thomas@kluyver.me.uk>
2017-01-24 16:27:38 +0000

    Update resource files with PyQt 5.5

--- nbmanager/ui_mainwindow.py
+++ nbmanager/ui_mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.4.2
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
