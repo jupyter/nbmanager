731b80b5c358de9247bc54f499a3279253f74fed
Peter Lebbing <peter@digitalbrains.com>
2018-08-13 14:30:53 +0200

    Fix xdg-install icon installation (#16)

--- xdg-install.sh
+++ xdg-install.sh
@@ -9,7 +9,7 @@ echo "Installing data files to: ${XDG_DATA_HOME:=$HOME/.local/share}"
 #export XDG_UTILS_DEBUG_LEVEL=1  #DEBUG
 
 for s in 48 64 128; do
-    xdg-icon-resource install --noupdate --size $s --context apps "icons/hicolor/apps/${size}x${size}/jupyter-nbmanager.png" jupyter-nbmanager
+    xdg-icon-resource install --noupdate --size $s --context apps "icons/hicolor/${s}x${s}/apps/jupyter-nbmanager.png" jupyter-nbmanager
 done
 xdg-icon-resource forceupdate
 
